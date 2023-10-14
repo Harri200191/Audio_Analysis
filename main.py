9fd3457d602ba666b5a56b1c4b55972d1fdcec31
haris rehman <harisrehmanchugtai@gmail.com>
2023-10-15 02:49:40 +0500

    Added main files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import os
+from pydub import AudioSegment
 import shutil
 
 def select_audio_file():
@@ -17,13 +18,31 @@ def select_audio_file():
     else:
         return None
     
+def extract_extension(input_string): 
+    last_period_index = input_string.rfind(".")
+ 
+    if last_period_index != -1: 
+        extension = input_string[last_period_index + 1:]
+        return extension
+    else: 
+        return input_string
+
+def convert_audio_to_mp3(input_path, output_path): 
+    audio = AudioSegment.from_file(input_path) 
+    audio.export(output_path, format="mp3")
+    
 def copy_to_current_directory(source_path, destination_directory):
     if not os.path.exists(destination_directory):
         os.makedirs(destination_directory)
 
     file_name = os.path.basename(source_path)
-    destination_path = os.path.join(destination_directory, file_name)
+    extens = extract_extension(file_name)
+    destination_path = os.path.join(destination_directory, "MainAudio."+extens)
     shutil.copy2(source_path, destination_path)
+    input_path = "MainAudio."+extens  
+    output_path = "MainAudio.mp3"
+    convert_audio_to_mp3(input_path, output_path)
+    
     return destination_path
 
 if __name__ == "__main__":
