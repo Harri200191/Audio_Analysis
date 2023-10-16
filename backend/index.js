546e7699777da96852785a3c49f00628ef2a4d9e
haris rehman <harisrehmanchugtai@gmail.com>
2023-10-16 22:01:51 +0500

    Added UI/UX files

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,4 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,10 +8,8 @@ app.get("/", (req, resp) => {
     resp.send("Home Page");
 });
 
-
 const startserver = async () => {
     try{
-        await connectDB();
         app.listen(PORT, () => {
             console.log("Server running on port: ", PORT);
         });
